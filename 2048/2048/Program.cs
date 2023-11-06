de0e03db116ab47ae736f20a390c30a1b4b98620
Diogo Martins <diogopro1400@gmail.com>
2023-11-06 11:27:49 +0100

    Création Tableau

--- 2048/2048/Program.cs
+++ 2048/2048/Program.cs
@@ -10,7 +10,19 @@ namespace _2048
     {
         static void Main(string[] args)
         {
-            safahsfjashfuhj
+            // Création du tableau
+
+            int[,] tableau = new int[4, 4];
+
+            for (int row = 0; row < 4; row++)
+            {
+                for (int col = 0; col < 4; col++) 
+                {
+                    Console.Write(tableau[row, col] + "\t");
+                }
+                Console.WriteLine("\n\n");
+            }
+            Console.ReadKey();
         }
     }
 }
